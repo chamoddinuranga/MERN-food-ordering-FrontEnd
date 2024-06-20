53f208c30074cf99116415847d1cd7b0aa1e66cb
chamoddinuranga <chamod3246@gmail.com>
2024-06-20 07:42:00 +0530

    auth0 added

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,9 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 const MainNav = () => {
+    const { loginWithRedirect } = useAuth0();
     return (
         <Button variant="ghost"
-            className="font-bold hover:text-[#44bd32] hover:bg-white">
+            className="font-bold hover:text-[#44bd32] hover:bg-white"
+            onClick={async () => await loginWithRedirect()}
+            >
             Log In
         </Button>
     )
